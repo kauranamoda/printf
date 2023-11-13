f0b8fae4423fd193844e899444afb9d9e76a898f
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2023-11-14 01:23:36 +0200

    printf

---
+++ _printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+int _printf(const char *format,...)
+int char_print = 0;
+va_list list_of_args;
+va_start;
+va_arg;
+va_end;
---
+++ main.h
@@ -0,0 +1,6 @@
+#define MAIN_H
+#ifndef MAIN_H
+#include <stdarg.h>
+#include <strlen.h>
+#include <unistd.h>
+#endif
